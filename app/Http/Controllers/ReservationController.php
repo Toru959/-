0bf45e5a9a73add5f8eca6bc742f0b42116d71a5
Toru959 <80747436+Toru959@users.noreply.github.com>
2024-07-12 01:19:10 +0800

    event reserve

---
+++ app/Http/Controllers/ReservationController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Event;
+use App\Models\Reservation;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+
+class ReservationController extends Controller
+{
+    public function dashboard()
+    {
+        return view('dashboard');
+    }
+
+    public function detail($id)
+    {
+        $event = Event::findOrFail($id);
+        $reservedPeople = DB::table('reservations')
+        ->select('event_id', DB::raw('sum(number_of_people) as number_of_people'))
+        ->whereNull('canceled_date')
+        ->groupBy('event_id')
+        ->having('event_id', $event->id)
+        ->first();
+
+        if(!is_null($reservedPeople))
+        {
+            $reservablePeople = $event->max_people - $reservedPeople->number_of_people;
+        } else {
+            $reservablePeople = $event->max_people;
+        }
+
+        return view('event-detail', compact('event', 'reservablePeople'));
+    }
+
+    public function reserve(Request $request)
+    {
+        //dd($request->reservedPeople);
+        $event = Event::findOrFail($request->id);
+        $reservedPeople = DB::table('reservations')
+        ->select('event_id', DB::raw('sum(number_of_people) as number_of_people'))
+        ->whereNull('canceled_date')
+        ->groupBy('event_id')
+        ->having('event_id', $request->id)
+        ->first();
+        //dd($reservedPeople); 
+
+        if(is_null($reservedPeople) || 
+        $event->max_people >= $reservedPeople->number_of_people + $request->reservedPeople)
+        {
+            Reservation::create([
+                'user_id' => Auth::id(),
+                'event_id' => $request->id,
+                'number_of_people' => $request->reservedPeople,
+            ]);
+
+            session()->flash('status', '登録OKです');
+            return to_route('dashboard');
+
+        } else {
+            session()->flash('status', 'この人数は予約できません');
+            return view('dashboard');
+        }
+    }
+}
