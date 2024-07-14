6f65dbc6560bfeacb716dae674c6d7ed79f47028
Toru959 <80747436+Toru959@users.noreply.github.com>
2024-07-14 13:51:54 +0800

    mypage

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,23 +1,24 @@
-<x-calendar-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            イベントカレンダー
-        </h2>
-    </x-slot>
+<x-app-layout>
+    <x-calendar-layout>
+        <x-slot name="header">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                イベントカレンダー
+            </h2>
+        </x-slot>
 
-    <div class="py-4">
-        <div class="event-calendar mx-auto sm:px-6 lg:px-8">
-            <div class="text-black bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                @if (session('status'))
-                    <div class="mb-4 font-memdium text-sm text-green-600">
-                        {{ session('status') }}
+        <div class="py-4">
+            <div class="event-calendar mx-auto sm:px-6 lg:px-8">
+                <div class="text-black bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                    @if (session('status'))
+                        <div class="mb-4 font-memdium text-sm text-green-600">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    @livewire('calendar')
+                    @livewireScripts <!-- これがないとinputの値ろコンポーネントで受け取れずカレンダーが更新しない。必須 -->
                     </div>
-                @endif
-                @livewire('calendar')
-                @livewireScripts <!-- これがないとinputの値ろコンポーネントで受け取れずカレンダーが更新しない。必須 -->
                 </div>
             </div>
         </div>
-    </div>
-</x-calendar-layout>
-            
+    </x-calendar-layout>
+</x-app-layout>      
